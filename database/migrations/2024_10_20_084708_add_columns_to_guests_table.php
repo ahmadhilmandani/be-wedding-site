05f71a641d06d75ab4981ef90f687b1c58ea156a
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-10-20 15:50:01 +0700

    add column in guest table

---
+++ database/migrations/2024_10_20_084708_add_columns_to_guests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('guests', function (Blueprint $table) {
+            $table->boolean('is_attend_marriage');
+            $table->boolean('is_attend_party');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('guests', function (Blueprint $table) {
+            $table->dropColumn('is_attend_marriage');
+            $table->dropColumn('is_attend_party');
+        });
+    }
+};
